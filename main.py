5932d31c7e4fa84c6a1781bbc26e1499f420a204
ovalent <ovalent@gmail.com>
2017-06-19 17:43:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,25 @@
 #qpy:3
 #qpy:console
 
-print("This is console module")
+import glob
+import numpy
+
+
+# List "lst" files in current folder
+sets = glob.glob("*.lst")
+print(sets)
+
+# Randomly select a set
+myset = numpy.random.choice(sets)
+print(myset)
+
+# open read selected set
+select_set = [line.rstrip('\n') for line in open(myset)]
+#print(select_set)
+
+
+# Randomly choose items from selected set
+choice_size = numpy.random.choice([1,2,3])
+#print(choice_size)
+choice = numpy.random.choice(select_set, size=choice_size, replace=False)
+print(choice)
