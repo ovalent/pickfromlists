f11ab98a8c24a61bea3259a1b242387990810ddd
ovalent <ovalent@gmail.com>
2017-06-30 12:08:51 +0200

    code moved to python 2.7

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
 #-*-coding:utf8;-*-
-#qpy:3
+#qpy:2
 #qpy:console
 
 import glob
 import numpy
+import os 
 
 
-# List "lst" files in current folder
-sets = glob.glob("*.lst")
-print(sets)
+#> List "lst" files in current folder
+full_path = os.path.realpath(__file__) 
+cur_dir = os.path.dirname(full_path)
+print("This file directory only") 
+#print(cur_dir)
+sets = glob.glob(cur_dir + "/*.lst")
+#print(sets)
 
-# Randomly select a set
+#> Randomly select a set
 myset = numpy.random.choice(sets)
-print(myset)
+#print(myset)
 
-# open read selected set
+#> open read selected set
 select_set = [line.rstrip('\n') for line in open(myset)]
 #print(select_set)
 
 
-# Randomly choose items from selected set
+#> Randomly choose items from selected set
 choice_size = numpy.random.choice([1,2,3])
 #print(choice_size)
 choice = numpy.random.choice(select_set, size=choice_size, replace=False)
