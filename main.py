fbebd03513300912adefae6f92388ac4379f9cd4
ovalent <ovalent@gmail.com>
2017-07-06 10:38:28 +0200

    add input loop

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 
 import glob
 import numpy
-import os 
+import os
 
 #> List "lst" files in current folder
 full_path = os.path.realpath(__file__) 
@@ -14,19 +14,27 @@ sets = glob.glob(cur_dir + "/*.lst")
 #print(sets)
 
 
+again = 1
+
+while again:
 
 
+    #> Randomly select a set
+    myset = numpy.random.choice(sets)
+    #print(myset)
+    
+    #> open read selected set
+    select_set = [line.rstrip('\n') for line in open(myset)]
+    #print(select_set)
+    
+    #> Randomly choose items from selected set
+    choice_size = numpy.random.choice([1,2,3])
+    #print(choice_size)
+    choice = numpy.random.choice(select_set, size=choice_size, replace=False)
+    print(choice)
 
-#> Randomly select a set
-myset = numpy.random.choice(sets)
-#print(myset)
+    is_again = raw_input("Press 'enter' to re-do ir again: ")
+    if is_again != "":
+        again = 0
 
-#> open read selected set
-select_set = [line.rstrip('\n') for line in open(myset)]
-#print(select_set)
 
-#> Randomly choose items from selected set
-choice_size = numpy.random.choice([1,2,3])
-#print(choice_size)
-choice = numpy.random.choice(select_set, size=choice_size, replace=False)
-print(choice)
